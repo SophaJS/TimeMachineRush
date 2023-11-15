67e8db2924429ad3dbde2abd2e3fcc87f3587121
Venora <venora10@gmail.com>
2023-11-15 17:29:46 -0500

    settings

--- ScriptFolder/settings_menu.gd
+++ ScriptFolder/settings_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var mute_music = $TabContainer/SettingsMenu/settingsmenu/CheckButtonMusic
+@onready var mute_sound_effects = $TabContainer/SettingsMenu/settingsmenu/CheckButtonSoundEffects
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +11,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_check_button_music_toggled(button_pressed):
+	pass # Replace with function body.
+
+
+func _on_check_button_sound_effects_toggled(button_pressed):
+	pass # Replace with function body.
