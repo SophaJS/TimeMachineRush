1978e009bc13ee41901e5277f624ec240e9e0f49
SophaJS <sophiaschatz2005@gmail.com>
2023-11-15 14:40:26 -0500

    Countdown

---
+++ CountDown.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var seconds = 30
+
+func _ready() -> void:
+	seconds = 20
+	$Seconds.text = str(seconds)
+
+func _on_timer_timeout():
+	if seconds == 0:
+		DoStuff()
+	else:
+		seconds -= 1
+		if seconds>=10:
+			$Seconds.text = str(seconds)
+		elif seconds>=0:
+			$Seconds.text = "0" + str(seconds)
+	
+func DoStuff() -> void:
+	get_tree().change_scene_to_file("res://SceneFolder/game_lose.tscn")
+	
